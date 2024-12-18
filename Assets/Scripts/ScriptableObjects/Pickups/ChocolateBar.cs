f64f852acbf038532bf907486eac9ae4f9202962
Emmanuel Williams <emmanuel.letton@gmail.com>
2024-12-18 13:50:22 +1030

    Initial framework for audio system and Chocolate Bar pickup.

---
+++ Assets/Scripts/ScriptableObjects/Pickups/ChocolateBar.cs
@@ -0,0 +1,17 @@
+///<summary>
+/// Author: Emmanuel
+///
+/// Holds the point values for chocolate bar pickups.
+///
+///</summary>
+
+using UnityEngine;
+
+namespace CadburyRunner
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/ChocolateBar", fileName = "ChocolateBar", order = 0)]
+    public class ChocolateBar : ScriptableObject
+    {
+        [SerializeField] private int m_pointValue;
+    }
+}
