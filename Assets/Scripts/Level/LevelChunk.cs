24804ca564a3050ea5eb9184f28821147ed954ec
Halen Finlay <halenfinlay@gmail.com>
2024-12-18 14:52:48 +1030

    Level Chunks speed can now be changed
    public method for setting the speed of the level chunks for slowing the player down and such.

--- Assets/Scripts/Level/LevelChunk.cs
+++ Assets/Scripts/Level/LevelChunk.cs
@@ -7,7 +7,7 @@
 
 using UnityEngine;
 
-namespace CadburyRunner.Generation
+namespace CadburyRunner.Level
 {
 	public class LevelChunk : MonoBehaviour
 	{
@@ -22,20 +22,22 @@ namespace CadburyRunner.Generation
 		{
 			for (int i = 0; i < m_obstaclePositions.Length - 1 ; i++)
 			{
-				Instantiate(m_possibleObstacles[Random.Range(0, m_possibleObstacles.Length - 1)], transform);
+				// roll to create obstacle
+				if (Random.value <= LevelMetrics.ObstacleSpawnChance)
+					Instantiate(m_possibleObstacles[Random.Range(0, m_possibleObstacles.Length - 1)], transform);
 			}
 		}
 
         private void Update()
         {
-			transform.Translate(new Vector3(0, 0, GenerationMetrics.LevelSpeed) * -Time.deltaTime);
+			transform.Translate(new Vector3(0, 0, LevelManager.Instance.CurrentLevelSpeed * -Time.deltaTime));
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
 			{
-				GenerationManager.Instance.GenerateNewChunk();
+				LevelManager.Instance.GenerateNewChunk();
 			}
         }
     }
---
+++ Assets/Scripts/Level/LevelMetrics.cs
@@ -0,0 +1,19 @@
+///<summary>
+/// Author: Halen
+///
+/// Constants for level generation.
+///
+///</summary>
+
+using UnityEngine;
+
+namespace CadburyRunner.Level
+{
+    public static class LevelMetrics
+    {
+        public static float Speed = 5f;
+        public static float Acceleration = 0.8f;
+        public static float Decceleration = 8f;
+        public static float ObstacleSpawnChance = 0.3f;
+    }
+}
