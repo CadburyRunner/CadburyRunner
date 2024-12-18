4ddca57df456e62101fb737edf8511140a44553c
Jaxon - AIE <126209068+Sk1ll1ssue@users.noreply.github.com>
2024-12-18 12:45:20 +1030

    character movement done
    you can now jump, slide, move left to right and cancel jump and slide

--- Assets/Scripts/PlayerControls/Charactermovement.cs
+++ Assets/Scripts/PlayerControls/Charactermovement.cs
@@ -7,6 +7,7 @@
 
 using UnityEngine;
 using CadburyRunner.Obstacle;
+using System.Collections;
 
 namespace CadburyRunner
 {
@@ -14,18 +15,98 @@ namespace CadburyRunner
 	{
 		public class CharacterMovement : MonoBehaviour
 		{
-            [SerializeField] private Vector3 m_leftFull;
-            [SerializeField] private Vector3 m_rightFull;
+            [Header("Float variables")]
+            [SerializeField] private float m_leftFull;
+            [SerializeField] private float m_rightFull;
             [SerializeField, Range(0, 1)] private float m_Input;
+            [SerializeField] private float m_slidingTime;
+            [SerializeField] private float forcePower;
+
+            [Header("References")]
+            [SerializeField] private GameObject m_normalCollider;
+            [SerializeField] private GameObject m_sliderCollider;
+
+            private Rigidbody m_rb;
+            private bool m_isSliding;
+            private bool m_isGrounded;
+
+            private void Start()
+            {
+                m_rb = GetComponent<Rigidbody>();
+            }
 
             void Update()
             {
+                m_isGrounded = Physics.Raycast(transform.position, Vector3.down, 1f, LayerMask.NameToLayer("Ground"));
+#if UNITY_EDITOR
                 SideToSideMovement(m_Input);
+                if (Input.GetKeyDown(KeyCode.S))
+                    Slide();
+
+                if (Input.GetKeyDown(KeyCode.W))
+                    Jump();
+#endif
             }
 
             public void SideToSideMovement(float input)
             {
-                transform.position = Vector3.Lerp(m_leftFull, m_rightFull, input);
+                transform.position = Vector3.Lerp(new Vector3(m_leftFull,transform.position.y,0), new Vector3(m_rightFull,transform.position.y,0), input);
+            }
+
+            public void Jump()
+            {
+                // if the player is grounded
+                if (m_isGrounded)
+                {
+                    // stop all coroutines which is only the sliding one
+                    StopAllCoroutines();
+
+                    // reset to normal
+                    m_isSliding = false;
+                    m_normalCollider.SetActive(true);
+                    m_sliderCollider.SetActive(false);
+
+                    // add force up
+                    m_rb.AddForce(Vector3.up * forcePower, ForceMode.Force);
+                }
+            }
+
+            public void Slide()
+            {
+                // if character is not already sliding
+                if (!m_isSliding)
+                {
+                    // if the character is in the air
+                    if (!m_isGrounded)
+                    {
+                        // just pull the character back down fast
+                        m_rb.AddForce(Vector3.down * forcePower, ForceMode.Force);
+                    }
+                    else
+                    {
+                        // if not in the air then switch collider to sliding collider
+                        m_isSliding = true;
+                        m_normalCollider.SetActive(false);
+                        m_sliderCollider.SetActive(true);
+
+                        // start the IsSliding coroutine
+                        StartCoroutine(IsSliding());
+                    }
+                }
+            }
+
+            private IEnumerator IsSliding()
+            {
+                // wait till the slide is done
+                yield return new WaitForSeconds(m_slidingTime);
+
+                // reset the colliders and bool
+                m_isSliding = false;
+                m_normalCollider.SetActive(true);
+                m_sliderCollider.SetActive(false);
+
+                // stop the coroutine
+                StopCoroutine(IsSliding());
             }
 
             public void GetHit(ObstacleType type)
