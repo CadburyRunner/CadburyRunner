01ce4d5770c6865551841fc96658e5c7f1f4dcde
Jaxon - AIE <126209068+Sk1ll1ssue@users.noreply.github.com>
2024-12-18 12:00:20 +1030

    get hit function
    GetHit function for Aiden to work with collision

--- Assets/Scripts/PlayerControls/Charactermovement.cs
+++ Assets/Scripts/PlayerControls/Charactermovement.cs
@@ -1,29 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+///<summary>
+/// Author: Jaxon Haldane
+/// Handles what happens to the character when input is sent through.
+/// does side to side movement,
+/// jumping and sliding
+///</summary>
+
 using UnityEngine;
 
-public class Charactermovement : MonoBehaviour
+namespace CadburyRunner
 {
-    [SerializeField] private Vector3 m_leftFull;
-    [SerializeField] private Vector3 m_rightFull;
-    [SerializeField, Range(0,1)] private float m_Input;
-    private bool m_isGrounded;
+	namespace Movement
+	{
+		public class CharacterMovement : MonoBehaviour
+		{
+            [SerializeField] private Vector3 m_leftFull;
+            [SerializeField] private Vector3 m_rightFull;
+            [SerializeField, Range(0, 1)] private float m_Input;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+            void Update()
+            {
+                SideToSideMovement(m_Input);
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        SideToSideMovement(m_Input);
-    }
+            public void SideToSideMovement(float input)
+            {
+                transform.position = Vector3.Lerp(m_leftFull, m_rightFull, input);
+            }
 
-    public void SideToSideMovement(float input)
-    {
-        transform.position = Vector3.Lerp(m_leftFull, m_rightFull, input);
-    }
+            public void GetHit()
+            {
+
+            }
+
+        }
+	}
 }
