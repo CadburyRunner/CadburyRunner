9b4f91c77c095b564da9118d18363f4ef65572ab
AidanProgramming <aidanmajcen@gmail.com>
2024-12-19 12:20:55 +1030

    Added new models and started ui
    - models have been updated and have added animations
    - started on ui work

---
+++ Assets/Scripts/PlayerHUD/CollectableBounce.cs
@@ -0,0 +1,37 @@
+///<summary>
+/// Author: Aidan
+///
+/// Fancy animated effect
+///
+///</summary>
+
+using UnityEngine;
+
+namespace CadburyRunner
+{
+	public class CollectableBounce : MonoBehaviour
+	{
+        [SerializeField] private GameObject m_collectableObject;
+
+
+        private float m_waveTime = 5f;
+
+        private void Update()
+        {
+            float scale = Mathf.Pow((float)System.Math.E, -m_waveTime) * Mathf.Cos(2 * Mathf.PI * m_waveTime) + 1;
+
+            m_collectableObject.transform.rotation = Quaternion.Euler(0, 0, 10f * Mathf.Sin(Time.time) + 23 - ((scale - 1) * 20f));
+
+            m_collectableObject.transform.localScale = new Vector3 (scale, scale, scale);
+
+            m_waveTime += Time.deltaTime * 2f;
+
+        }
+
+
+        public void Collected()
+        {
+            m_waveTime = 0f;
+        }
+    }
+}
